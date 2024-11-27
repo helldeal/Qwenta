dbfdedb507c18465f3b46e030653039f7e78e41c
Alexandre Clenet <alexandreclenet@free.fr>
2024-11-27 11:42:21 +0100

    setup nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-  return <></>;
-}
+import { BrowserRouter, Routes, Route } from "react-router";
+import OnBoarding from "./routes/OnBoarding";
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<OnBoarding />} />
+        <Route path="/Dashboard" element={<OnBoarding />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
